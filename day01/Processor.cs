9747e808f9da76475f7a18527e3bae85e6527cfa
Marcin Kamiński <marcin.kaminski.gd@gmail.com>
2024-11-30 22:40:57 +0100

    Create solution and first project

---
+++ day01/Processor.cs
@@ -0,0 +1,39 @@
+﻿internal class Processor
+{
+
+    List<string> lines = new List<string>();
+
+    internal void ProcessTask1(string @filename)
+    {
+        int sum = 0;
+        lines.Clear();
+        foreach (string line in File.ReadLines(@filename))
+        {
+            lines.Add(line);
+        }
+        
+        //??
+
+
+        Console.WriteLine($"RESULT: {sum}");
+
+    }
+
+
+    internal void ProcessTask2(string @filename)
+    {
+        int sum = 0;
+        lines.Clear();
+        foreach (string line in File.ReadLines(@filename))
+        {
+            lines.Add(line);
+        }
+
+        //??
+
+
+        Console.WriteLine($"RESULT: {sum}");
+
+    }
+
+}
---
+++ day01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Processor p = new Processor();
+
+
+        p.ProcessTask1("testdata.txt");
+        p.ProcessTask1("inputdata.txt");
+
+
+        //p.ProcessTask2("testdata.txt");
+        //p.ProcessTask2("inputdata.txt");
+
+
+    }
+}
