9f5c5eb985bcc32fcef5fe5de2a79d6a053be8f5
Marcin Kamiński <marcin.kaminski.gd@gmail.com>
2024-12-02 22:37:53 +0100

    project 3 scaffold

---
+++ day03/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Processor p = new Processor();
+
+
+        Console.WriteLine("First part");
+        p.ProcessTask1("testdata.txt");
+        //p.ProcessTask1("inputdata.txt");
+
+
+        //Console.WriteLine("Second part");
+        //p.ProcessTask2("testdata.txt");
+        //p.ProcessTask2("inputdata.txt");
+
+
+    }
+}
