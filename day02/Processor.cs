978cf2ec926e91e8c15effd9b0527794dda3d410
Marcin Kamiński <marcin.kaminski.gd@gmail.com>
2024-12-02 06:22:47 +0100

    Complete day02

---
+++ day02/Processor.cs
@@ -0,0 +1,143 @@
+﻿
+
+internal class Processor
+{
+    List<Report> reports = new List<Report>();
+
+    internal void ProcessTask1(string @filename)
+    {
+        int sum = 0;
+        reports.Clear();
+        foreach (string line in File.ReadLines(@filename))
+        {
+            //levels...
+            string[] levels = line.Split(' ');
+            //5 of them...
+            Report report = new Report();
+            foreach (string level in levels)
+            {
+                report.Levels.Add(int.Parse(level));
+            }
+
+            reports.Add(report);
+        }
+
+        foreach (Report report in reports)
+        {
+            if (report.IsSafe()) sum++;
+        }
+
+        Console.WriteLine($"RESULT: {sum}");
+    }
+
+    internal void ProcessTask2(string @filename)
+    {
+        int sum = 0;
+        reports.Clear();
+        foreach (string line in File.ReadLines(@filename))
+        {
+            //levels...
+            string[] levels = line.Split(' ');
+            //5 of them...
+            Report report = new Report();
+            foreach (string level in levels)
+            {
+                report.Levels.Add(int.Parse(level));
+            }
+            reports.Add(report);
+        }
+
+        foreach (Report report in reports)
+        {
+            if (report.IsSafe()) sum++;
+            else if (report.CheckSubReports()) sum++;
+        }
+
+        Console.WriteLine($"RESULT: {sum}");
+    }
+
+}
+
+internal class Report
+{
+    public List<int> Levels = new List<int>();
+
+    internal bool IsSafe()
+    {
+        bool increasing = CheckIsIncresing();
+        bool decreasing = CheckIsDecreasing();
+
+        int maxDifference = GetMaxDifference();
+
+        return (increasing || decreasing)
+            &&
+            (1 <= maxDifference  && maxDifference<=3);
+    }
+
+    private int GetMaxDifference()
+    {
+        //get max difference between levels
+        int diff = 0;
+        for (int i = 0; i < Levels.Count - 1; i++)
+        {
+            int current = Levels[i];
+            int next = Levels[i + 1];
+            int currentDiff = Math.Abs(current - next);
+            if (currentDiff > diff)
+            {
+                diff = currentDiff;
+            }
+        }
+        return diff;
+    }
+
+    private bool CheckIsDecreasing()
+    {
+        //check if levels are decreasing
+        for (int i = 0; i < Levels.Count - 1; i++)
+        {
+            int current = Levels[i];
+            int next = Levels[i + 1];
+            if (current >= next)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private bool CheckIsIncresing()
+    {
+        for (int i = 0; i < Levels.Count - 1; i++)
+        {
+            int current = Levels[i];
+            int next = Levels[i + 1];
+            if (current <= next)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    internal bool CheckSubReports()
+    {
+        for (int i = 0; i < Levels.Count; i++)
+        {
+            Report subReport = new Report();
+            for (int j = 0; j < Levels.Count; j++)
+            {
+                if (j != i)
+                {
+                    subReport.Levels.Add(Levels[j]);
+                }
+            }
+            //check if subreport is safe
+            if (subReport.IsSafe())
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ day02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Processor p = new Processor();
+
+
+        Console.WriteLine("First part");
+        p.ProcessTask1("testdata.txt");
+        p.ProcessTask1("inputdata.txt");
+
+
+        Console.WriteLine("Second part");
+        p.ProcessTask2("testdata.txt");
+        p.ProcessTask2("inputdata.txt");
+
+
+    }
+}
